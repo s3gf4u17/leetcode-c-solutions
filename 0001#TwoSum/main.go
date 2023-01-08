fad09231dbe95c8f2aa924212dd903bbcb7bfecd
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-08 13:48:10 +0100

    two sum improved

--- 0001#TwoSum/main.go
+++ 0001#TwoSum/main.go
@@ -1,14 +1,13 @@
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-    // i cannot sort it because then ill mix the initial indices lmao
-    // sort.Ints(nums)
-    for i:=0;i<len(nums)-1;i++{
-        for j:=i+1;j<len(nums);j++{
-            if nums[i]+nums[j] == target {return []int{i,j}}
-            // break condition doesnt work on unsorted array
-            // if nums[i]+nums[j] > target {break}
+    hashtable := make(map[int]int)
+    for i,num := range nums {
+        if index,ok:=hashtable[num];ok {
+            return []int{index,i}
         }
+        hashtable[target-num]=i
     }
+    fmt.Println(hashtable)
     return []int{0,0}
 }
