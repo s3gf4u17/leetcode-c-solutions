3d248be2acbfefd29a7c229e3babcb3762aeb71f
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-08 23:15:21 +0100

    improved: runtime 96% memory 12%

--- 0004#MedianOfTwoSortedArrays/main.go
+++ 0004#MedianOfTwoSortedArrays/main.go
@@ -1,28 +1,17 @@
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
     p1,p2:=0,0
     merged:=[]int{}
-    for p1<len(nums1) || p2<len(nums2) {
-        if p1>=len(nums1) && p2<len(nums2) {
-            merged=append(merged,nums2[p2])
-            p2++
-        } else if p1<len(nums1) && p2>=len(nums2) {
+    for p1<len(nums1) && p2<len(nums2) {
+        if nums1[p1] < nums2[p2] {
             merged=append(merged,nums1[p1])
             p1++
-        } else if p2<len(nums2) && p1<len(nums1) {
-            if nums1[p1]>nums2[p2] {
-                merged=append(merged,nums2[p2])
-                p2++
-            } else {
-                merged=append(merged,nums1[p1])
-                p1++
-            }
         } else {
-            break
+            merged=append(merged,nums2[p2])
+            p2++
         }
     }
-    if (len(nums1)+len(nums2))%2==0 {
-        return float64(merged[(len(nums1)+len(nums2))/2]+merged[(len(nums1)+len(nums2))/2-1])/2
-    } else {
-        return float64(merged[(len(nums1)+len(nums2))/2])
-    }
+    for p1=p1;p1<len(nums1);p1++ {merged=append(merged,nums1[p1])}
+    for p2=p2;p2<len(nums2);p2++ {merged=append(merged,nums2[p2])}
+
+    if len(merged)%2==0 {return float64(merged[len(merged)/2]+merged[(len(merged)-1)/2])/2} else {return float64(merged[len(merged)/2])}
 }
